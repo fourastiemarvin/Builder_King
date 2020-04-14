e73a3af0940f8f2fd4f7c237c47d9a70f770e066
Patrick  Alexandre Sardinha <patrick.sardinha@etu.unige.ch>
2020-04-15 01:40:24 +0200

    remove test assets & add player animation - missing fly anim & add script controller

---
+++ BuilderKing_v1.0.0/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      // FORWARD WALKING
+      if (Input.GetKey("w") || Input.GetKey(KeyCode.UpArrow)) {
+        anim.SetBool("isWalking", true);
+      }
+      else {
+        anim.SetBool("isWalking", false);
+      }
+
+      // BACKWARD WALKING
+      if (Input.GetKey("s") || Input.GetKey(KeyCode.DownArrow)) {
+        anim.SetBool("isBackwardWalking", true);
+      }
+      else {
+        anim.SetBool("isBackwardWalking", false);
+      }
+
+      // LEFT WALKING
+      if (Input.GetKey("a") || Input.GetKey(KeyCode.LeftArrow)) {
+        anim.SetBool("isLeftWalking", true);
+      }
+      else {
+        anim.SetBool("isLeftWalking", false);
+      }
+
+      // RIGHT WALKING
+      if (Input.GetKey("d") || Input.GetKey(KeyCode.RightArrow)) {
+        anim.SetBool("isRightWalking", true);
+      }
+      else {
+        anim.SetBool("isRightWalking", false);
+      }
+
+      // SELECTING OBJECT
+      if (Input.GetMouseButton(0)) {
+        anim.SetBool("isSelecting", true);
+      }
+      else {
+        anim.SetBool("isSelecting", false);
+      }
+    }
+}
---
+++ BuilderKing_v1.0.0/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float movementSpeed;
+    public float minY = 0.0f, maxY = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+      Vector3 currentPosition = transform.position;
+      currentPosition.y = Mathf.Clamp(currentPosition.y, minY, maxY);
+      transform.position = currentPosition;
+
+      if (Input.GetKey("w") || Input.GetKey(KeyCode.UpArrow)) {
+        transform.position += transform.TransformDirection(Vector3.forward) * Time.deltaTime * movementSpeed;
+      }
+      else if (Input.GetKey("s") || Input.GetKey(KeyCode.DownArrow)) {
+        transform.position -= transform.TransformDirection(Vector3.forward) * Time.deltaTime * movementSpeed;
+      }
+
+      if (Input.GetKey("a") || Input.GetKey(KeyCode.LeftArrow)) {
+        transform.position += transform.TransformDirection(Vector3.left) * Time.deltaTime * movementSpeed;
+      }
+      else if (Input.GetKey("d") || Input.GetKey(KeyCode.RightArrow)) {
+        transform.position -= transform.TransformDirection(Vector3.left) * Time.deltaTime * movementSpeed;
+      }
+
+      if (Input.GetKey("q")) {
+        transform.position += transform.TransformDirection(Vector3.up) * Time.deltaTime * movementSpeed;
+      }
+      else if (Input.GetKey("e")) {
+        transform.position -= transform.TransformDirection(Vector3.up) * Time.deltaTime * movementSpeed;
+      }
+
+
+      // FIXME: rotate the player with the camera
+      //transform.Rotate(Camera.main.transform.up, Input.GetAxis("Mouse X"));
+
+    }
+}
