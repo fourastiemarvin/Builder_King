32273efa766e2d84f8d691694473c9dd755d634e
fourastiemarvin <marvin.fourastie@etu.unige.ch>
2020-04-19 14:02:33 +0200

    fix phisics of the objects

--- BuilderKing_v1.0.0/Assets/Scripts/DragObject.cs
+++ BuilderKing_v1.0.0/Assets/Scripts/DragObject.cs
@@ -2,12 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DragObject : MonoBehaviour
-{
+public class DragObject : MonoBehaviour {
 
     private Vector3 mOffset;
     private float mZCoord;
+    public Rigidbody rb;
 
+    // Start is called before the first frame update
+    void Start(){
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Called when mouse key is pressed
     void OnMouseDown() {
       mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
       // Store offset = gameObject world pos - mouse world pos;
@@ -24,7 +30,14 @@ public class DragObject : MonoBehaviour
       return Camera.main.ScreenToWorldPoint(mousePoint);
     }
 
+    // called when the object is dragged
     void OnMouseDrag() {
       transform.position = GetMouseWorldPos() + mOffset;
+      rb.isKinematic = true;
+    }
+
+    // called when the mouse key is released
+    void OnMouseUp(){
+      rb.isKinematic = false;
     }
 }
